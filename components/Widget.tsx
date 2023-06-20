e80dd45137349329e155022c951bcdbf5f992be7
QuangNHN <quangnhn@glotechvn.com>
2023-06-20 16:21:03 +0700

    done phone

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Widget = () => {
@@ -8,6 +9,13 @@ const Widget = () => {
             <a href="tel:+84869073435" className="relative flex items-center justify-center">
                 <div className="absolute w-14 h-14 rounded-full animate-[zoomInZoomOut_1s_ease-in-out_infinite] bg-[#9addf5] z-10" />
                 <div className="absolute w-9 h-9 rounded-md bg-[#0c784c] z-20" />
+                <Image 
+                  src="/images/phone1.svg"
+                  alt="phone"
+                  width={28}
+                  height={28}
+                  className="absolute z-30"
+                  />
             </a>
         </div>
     </div>
