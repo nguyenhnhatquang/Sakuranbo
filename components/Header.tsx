b032818a04c0b2e157cfd15c650ba20866ff1a8b
QuangNHN <quangnhn@glotechvn.com>
2023-06-20 10:12:13 +0700

    Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -19,7 +19,6 @@ const Header = () => {
                 className="mr-2 text-xs"
               />
               <span>nguyenhnhatquang@gmail.com</span>
-              
             </a>
           </div>
           <div className="w-[50%] float-left flex justify-end">
@@ -29,7 +28,31 @@ const Header = () => {
           </div>
         </div>
       </aside>
-      <header>{/* Logo */}</header>
+      <header className="container flex item-center justify-center md:justify-between my-1">
+        <div className="flex items-center justify-center">
+          <Image
+            src="https://res.cloudinary.com/nguyenhnhatquang/image/upload/v1687226907/Sakuranbo/y87xsw0pkggbrmyen2ru.svg"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="mr-0 lg:mr-3"
+          />
+          <div className="hidden lg:block">
+            <h1 className="text-3xl">TRUNG TÂM NHẬT NGỮ SAKURANBO</h1>
+            <h3 className="text-lg">日本語センター</h3>
+          </div>
+        </div>
+        <div className="hidden md:flex items-center justify-center">
+          <Image
+            src="/images/phone.svg"
+            alt="Phone"
+            width={50}
+            height={50}
+            className="mr-0 lg:mr-2"
+          />
+          <a href="tel:+84869073435" className="text-xl hover:text-[#fc1010]">0869073435</a>
+        </div>
+      </header>
     </>
   );
 };
